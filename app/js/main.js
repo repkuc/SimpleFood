e8ea908e878ea132bae75f6d1573c1359265e29a
repkuc <arturs.gerkis@inbox.lv>
2023-06-25 07:01:29 +0300

    addet slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
-$(function () {});
+$(function () {
+  $(".review-slider__inner").slick({
+    dots: true,
+    autoplay: true,
+    // fade: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/icons/left-arrow.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/icons/right-arrow.svg" alt=""></button>',
+  });
+});
